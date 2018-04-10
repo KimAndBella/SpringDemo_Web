846c110963697ee37744439bd6788f887b1ff420
KimAndBella <1163927457@qq.com>
2018-04-11 01:22:55 +0800

    模板渲染没有成功，继续查明原因

--- src/main/java/com/kim/SpringbootdemoWebApplication.java
+++ src/main/java/com/kim/SpringbootdemoWebApplication.java
@@ -2,11 +2,26 @@ package com.kim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan
 public class SpringbootdemoWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoWebApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootdemoWebApplication.class, args);
+//		printBeanNames(applicationContext);
+	}
+	public static void printBeanNames(ApplicationContext applicationContext){
+		if(applicationContext != null){
+			String[] beanNames = applicationContext.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for(String name : beanNames){
+				System.out.println("definition name - "+name);
+			}
+		}
 	}
 }
--- src/main/java/com/kim/controller/LoginController.java
+++ src/main/java/com/kim/controller/LoginController.java
@@ -3,9 +3,10 @@ package com.kim.controller;
 import com.kim.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -22,13 +23,12 @@ public class LoginController {
         return "index";
     }
 
-    @PostMapping("/login")
+    @RequestMapping(value = "/login",method= RequestMethod.POST)
     public String login(@RequestParam("name")String username, @RequestParam("password")String password, ModelMap modelMap){
         System.out.println("login-run");
-       boolean result = loginService.loginCheck(username,password);
-       if(result){
-           System.out.println("-----");
-           modelMap.put("name",username);
+       if(loginService.loginCheck(username,password)){
+           System.out.println("-----username = "+username);
+           modelMap.addAttribute("name",username);
            return "home";
        }else{
            return "";
